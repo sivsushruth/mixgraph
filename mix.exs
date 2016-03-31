77f60eef1e6355fc1247e8c54f262deee3d6386d
Sushruth <sivsushruth@gmail.com>
2016-04-01 02:33:56 +0530

    Update package details

--- mix.exs
+++ mix.exs
@@ -8,25 +8,28 @@ defmodule Mixgraph.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      escript: [main_module: Mixgraph],
+     package: package,
      deps: deps]
   end
+  
+  defp package do
+    [
+      maintainers: ["Sushruth S"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sivsushruth/mixgraph"}
+    ]
+  end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  defp description do
+    """
+    Create an interactive dependency graph for any hex package published in hex.pm
+    """
+  end
+
   def application do
     [applications: [:logger, :httpotion, :json]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpotion, "~> 2.2.0"}, {:json, "~> 0.3.0"}]
   end
